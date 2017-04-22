ac47ea23f04dbf359190544b66592d673d3d3ba4
duke-leeth <duke.leeth@gmail.com>
2017-04-22 01:25:20 -0700

    deleted redundant method and variable

--- Agent.java
+++ Agent.java
@@ -1,41 +1,16 @@
 import java.lang.*;
 
 public class Agent{
-//  private Coordinate initialLocation = new Coordinate(0, 0);
-//  private String initialDirection = "N";
   private Coordinate location = new Coordinate(0, 0);
   private String direction = "N";
   private int boundry = 0;
 
-
   public Agent() {}
 
-/*
-  public Agent(Coordinate initialLocation
-                , String initialDirection
-                , int boundry) {
-    int[] locationArray = initialLocation.getCoordinateArrayFormat();
-    this.initialLocation = new Coordinate(locationArray[0], locationArray[1]);
-    this.setLocation(locationArray[0], locationArray[1]);
-    this.initialDirection = initialDirection;
-    this.setDirection(initialDirection);
-    this.boundry = boundry;
-  }
-*/
-
-/*
-  public String getInitialLocation() {
-    return this.initialLocation.getCoordinateStringFormat();
-  }
-*/
   public String getLocation() {
     return location.getCoordinateStringFormat();
   }
-/*
-  public String getInintialDirection() {
-    return this.initialDirection;
-  }
-*/
+
   public String getDirection() {
     return direction;
   }
@@ -43,15 +18,7 @@ public class Agent{
   public int getBoundry() {
     return boundry;
   }
-/*
-  public void setInitialLocation(int positionX, int positionY) {
-    if ( !locationIsValid(positionX, positionY) ) {
-      throw new IllegalArgumentException("Invalid location within the boundry");
-    }
-    this.initialLocation = new Coordinate(positionX, positionY);
-    this.setLocation(positionX, positionY);
-  }
-*/
+
   public void setLocation(int positionX, int positionY) {
     Coordinate newLocation = new Coordinate(positionX, positionY);
     if ( !locationIsValid(positionX, positionY) ) {
@@ -61,15 +28,7 @@ public class Agent{
     }
     this.location = newLocation;
   }
-/*
-  public void setInitialDirection(String direction) {
-    if ( !directionIsValid(direction) ) {
-      throw new IllegalArgumentException("Invalid direction: " + direction);
-    }
-    this.initialDirection = direction;
-    this.setDirection(direction);
-  }
-*/
+
   public void setDirection(String direction) {
     if ( !directionIsValid(direction) ) {
       throw new IllegalArgumentException("Invalid direction: " + direction);
@@ -127,7 +86,6 @@ public class Agent{
       location.setCoordinate(originalCoordArray);
     }
   }
-
 
   // Helper function
   private boolean locationIsValid(Coordinate coord) {
